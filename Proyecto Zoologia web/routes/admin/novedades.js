68623fda86b9f78f533ce79ede1c508d4bbd0b69
franco lencinas <francoezequiel876@gmail.com>
2023-08-07 22:59:14 -0300

    proyecto final

--- Proyecto Zoologia web/routes/admin/novedades.js
+++ Proyecto Zoologia web/routes/admin/novedades.js
@@ -21,5 +21,66 @@ router.get('/', async function(req, res, next) {
 
   }); 
 
+ router.get('/agregar', (req,res,next) => {
+   res.render('admin/agregar', {
+      layout: 'admin/layout'
+   })
+ });
+ 
+ router.post('/agregar', async (req,res,next) =>{
+   try {
+      if (req.body.titulo !="" && req.body.subtitulo !="" && req.body.cuerpo !="") {
+         await novedadesmodel.insertNovedades(req.body);
+         res.redirect('/admin/novedades')
+      } else {
+         res.render('admin/agregar', {
+            layout: 'admin/agregar',
+            error: true,
+            message: 'Todos los campos son requeridos'
+         })
+      }
+   } catch (error) {
+      console.log(error)
+      res.render('admin/agregar', {
+         layout: 'admin/layout',
+         error: true,
+         message: 'No se cargo la novedad'
+      })
+   }
+ })
+ 
+
+ router.get('/modificar/:id', async (req, res, next) => {
+   var id = req.params.id;
+   var novedad = await novedadesmodel.getNovedadById(id);
+
+   res.render('admin/modificar', {
+      layout: 'admin/layout',
+      novedad
+   });
+ });
+ 
+
+ router.post('/modificar', async (req, res, next) => {
+   try {
+      var obj = {
+         titulo: req.body.titulo,
+         subtitulo:req.body.subtitulo,
+         cuerpo:req.body.cuerpo
+      }
+      console.log(obj)
+      await novedadesmodel.modificarNovedadById(obj, req.body.id);
+      res.redirect('/admin/novedades');
+   } catch(error) {
+      console.log(error)
+      res.render('/admin/modificar', {
+         layout: 'admin/layout',
+         error: true,
+         message: 'No se modifico la novedad'
+      })
+   }
+ })
+ 
+ 
   module.exports = router;
   
